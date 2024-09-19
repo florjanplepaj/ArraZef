7864b4413ae403cedda4f555cbe37005a304db88
Florjan Plepaj <florjan.plepaj@gmail.com>
2024-09-19 17:19:03 +0200

    Fix the adding numbers

--- scripts/home.js
+++ scripts/home.js
@@ -16,3 +16,32 @@ function renderCoverGrid() {
 
   document.querySelector(".js-recent-projects-body").innerHTML = coverHtml;
 }
+
+statisticNumbers();
+function statisticNumbers() {
+  startCounter(".js-years-experience", 1, 15, 1, 30);
+  startCounter(".js-employers-over-the-years", 1, 170, 5, 30);
+  startCounter(".js-residential-complete", 1, 690, 10, 30);
+  startCounter(".js-millions-investment-complete", 1, 370, 5, 30);
+}
+
+function startCounter(elementId, start, target, increment, duration) {
+  let counter = start;
+
+  let interval = setInterval(function () {
+    const element = document.querySelector(elementId);
+
+    if (element) {
+      element.innerHTML = counter;
+      counter += increment;
+
+      if (counter > target) {
+        clearInterval(interval);
+        element.innerHTML = target;
+      }
+    } else {
+      clearInterval(interval);
+      console.error(`Element not found: ${elementId}`);
+    }
+  }, duration);
+}
